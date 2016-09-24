aab0f6d562e04e45cc19d3d490202f8ec3757968
Daniel Varga <dvarga@skawa.hu>
2016-09-24 18:48:30 +0200

    new util function: extract video id

--- lib/src/util.dart
+++ lib/src/util.dart
@@ -8,17 +8,22 @@ library youtube_iframe.utils;
 /// See [API Docs](https://developers.google.com/youtube/iframe_api_reference#Queueing_Functions)
 String getQualifiedPlayerUrl(String url) {
   Uri ytUrl = Uri.parse(url);
-  String id;
-  if (ytUrl.host == 'www.youtue.be' || ytUrl.host == 'youtue.be') {
-    // in the format of https://youtu.be/k2rcM3wwsS0
-    id = ytUrl.pathSegments.first;
-  } else
-  if ((ytUrl.host == 'youtube.com' || ytUrl.host == 'www.youtube.com') && ytUrl.queryParameters.containsKey('v')) {
-    id = ytUrl.queryParameters['v'];
-  } else {
-    throw new ArgumentError('Can\'t produce qualified YouTube player URL from: $url');
-  }
+  String id = extractVideoId(url);
   return new Uri.https('youtube.com', '/v/$id', {
     'version': '3'
   }).toString();
+}
+
+String extractVideoId(String url) {
+  Uri ytUrl = Uri.parse(url);
+  if (ytUrl.host == 'www.youtue.be' || ytUrl.host == 'youtue.be') {
+    // in the format of https://youtu.be/k2rcM3wwsS0
+    return ytUrl.pathSegments.first;
+  }
+
+  if ((ytUrl.host == 'youtube.com' || ytUrl.host == 'www.youtube.com') && ytUrl.queryParameters.containsKey('v')) {
+    return ytUrl.queryParameters['v'];
+  } else {
+    throw new ArgumentError('Can\'t produce qualified YouTube player URL from: $url');
+  }
 }
